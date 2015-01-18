c88fe6ca8a40b7168a8807ad3db14e1e9f61409b
jordanbecerra <jordanbecerra5@gmail.com>
2015-01-17 21:17:35 -0700

    Tried to add app.post with little success

--- app/routes.js
+++ app/routes.js
@@ -23,4 +23,28 @@ module.exports = function(app, passport) {
 	app.get('/womens', function(req, res) {
 		res.sendfile('./public/html/womens.html');
 	});
+	
+	app.post('/stripe', function(req, res) {
+	
+		// Set your secret key: remember to change this to your live secret key in production
+		// See your keys here https://dashboard.stripe.com/account
+		var stripe = require("stripe")("sk_test_SCMdCsdwdw2uUeLZeV01pYKj");
+		
+		console.log(req.body);
+
+		// (Assuming you're using express - expressjs.com)
+		// Get the credit card details submitted by the form
+		var stripeToken = request.body.stripeToken;
+
+		var charge = stripe.charges.create({
+		  amount: 1000, // amount in cents, again
+		  currency: "usd",
+		  card: stripeToken,
+		  description: "payinguser@example.com"
+		}, function(err, charge) {
+		  if (err && err.type === 'StripeCardError') {
+			// The card has been declined
+		  }
+		});
+	});
 };
