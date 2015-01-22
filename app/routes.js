faa0990fe9aaa8100187ffa56e10149bfbbf9bce
jordanbecerra <jordanbecerra5@gmail.com>
2015-01-22 16:06:29 -0700

    charges the correct amount depending on how many shirts are in the cart, kinda ugly

--- app/routes.js
+++ app/routes.js
@@ -56,15 +56,22 @@ module.exports = function(app, passport) {
 		// Set your secret key: remember to change this to your live secret key in production
 		// See your keys here https://dashboard.stripe.com/account
 		var stripe = require("stripe")("sk_test_SCMdCsdwdw2uUeLZeV01pYKj");
-
-		console.log(req.body.cartData);
+				
+		var cart = JSON.parse(req.body.cartData);
+		console.log(cart);
 
 		// (Assuming you're using express - expressjs.com)
 		// Get the credit card details submitted by the form
 		var stripeToken = req.body.stripeToken;
 
 		var charge = stripe.charges.create({
-		  amount: 1000,//cart.blueTell.small * 2500 + cart.blueTell.medium * 2500 + cart.blueTell.large * 2500 + cart.blueTell.xlarge * 2500, // amount in cents, again
+		  amount: cart.blueTell.small * 2500 + cart.blueTell.medium * 2500 + cart.blueTell.large * 2500 + cart.blueTell.xlarge * 2500
+		  		  + cart.creamHappy.small * 2500 + cart.creamHappy.medium * 2500 + cart.creamHappy.large * 2500 + cart.creamHappy.xlarge * 2500
+		  		  + cart.lightBlueThunder.small * 2500 + cart.lightBlueThunder.medium * 2500 + cart.lightBlueThunder.large * 2500 + cart.lightBlueThunder.xlarge * 2500
+		  		  + cart.pink.small * 2500 + cart.pink.medium * 2500 + cart.pink.large * 2500 + cart.pink.xlarge * 2500
+		  		  + cart.whiteThunder.small * 2500 + cart.whiteThunder.medium * 2500 + cart.whiteThunder.large * 2500 + cart.whiteThunder.xlarge * 2500
+		  		  + cart.stripeDrama.small * 2500 + cart.stripeDrama.medium * 2500 + cart.stripeDrama.large * 2500 + cart.stripeDrama.xlarge * 2500
+		  		  + cart.yellowDrama.small * 2500 + cart.yellowDrama.medium * 2500 + cart.yellowDrama.large * 2500 + cart.yellowDrama.xlarge * 2500, // amount in cents, again
 		  currency: "usd",
 		  card: stripeToken,
 		  description: "payinguser@example.com"
