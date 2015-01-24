aae3bc5227310f7d1591b015b632e753e511bea1
Dayne Davis <daynedavis5@gmail.com>
2015-01-24 10:37:51 -0800

    Added shipping address to admin page

--- public/js/admin.js
+++ public/js/admin.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   var orders = JSON.parse(ordersText.responseText);
 	for (var i = 0; i < orders.length; i++) {
 		var cart = JSON.parse(orders[i].cart);
-		$('#ListedOrders').append("<table><thead><tr><th colspan='3'>" + orders[i].name + "</th></tr><tr><th>Item</th><th>Size</th><th>Quantity</th></tr></thead><tbody id='checkoutBody" + i +"'></tbody></table>");
+		$('#ListedOrders').append("<table><thead><tr><th colspan='3'>" + orders[i].name + " | " + orders[i].email + "</th></tr><tr><td colspan='3'>" + orders[i].shippingAddressLine1 + " " + orders[i].shippingAddressApt + " " + orders[i].shippingAddressCity + ", " + orders[i].shippingAddressState + " " + orders[i].shippingAddressZip + "</td></tr><tr><th>Item</th><th>Size</th><th>Quantity</th></tr></thead><tbody id='checkoutBody" + i +"'></tbody></table>");
 		for (var shirt in cart) {
 			var sizes = cart[shirt];
 			for (var size in sizes) {
