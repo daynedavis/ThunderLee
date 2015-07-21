3f8b63f9cbb9e07b13cd542112e079839455a330
jordanbecerra <jordanbecerra5@gmail.com>
2015-07-21 13:04:12 -0700

    cleaned up comments from server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // set up ======================================================================
 var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
+var app      = express();
+var mongoose = require('mongoose');
+var port  	 = process.env.PORT || 8080;
 var passport = require('passport');
 var flash = require('connect-flash');
-var database = require('./config/database'); 			// load the database config
+var database = require('./config/database');
 var morgan   = require('morgan');
 var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
@@ -23,22 +23,16 @@ var options = {
 };
 mongoose.connect(database.url, options);
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(cookieParser());
-
-/*app.use(session({ secret: 'ilovescotchscothcyscotchdayne'}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());*/
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport, nodemailer);
-//require('./config/passport')(passport);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
